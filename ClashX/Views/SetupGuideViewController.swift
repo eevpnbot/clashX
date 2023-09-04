e4170434fba3ef57ccacdfd0b21537a0ddde4c44
miniLV <liangyuhangapple@gmail.com>
2023-09-04 14:04:26 +0800

    misc: create Setup Guide tabview

---
+++ ClashX/Views/SetupGuideViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SetupGuideViewController.swift
+//  ClashX
+//
+//  Created by miniLV on 2023-09-04.
+//  Copyright © 2023 ClashX. All rights reserved.
+//
+
+import Cocoa
+
+class SetupGuideViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+
+}
